4d17690670c0491251cc05d898631b9904eae7b8
matsbyfl <mats@byfuglien.net>
2019-01-09 17:06:59 +0100

    deleted console.log

--- api/src/controllers/getroles.js
+++ api/src/controllers/getroles.js
@@ -4,8 +4,6 @@ const PROD_OPERATIONS_GROUPS = process.env['PROD_OPERATIONS_GROUPS']
   .map(elem => elem.trim())
 const OPERATION_GROUPS = process.env['OPERATION_GROUPS'].split(',').map(elem => elem.trim())
 
-console.log('BABV ', SUPERUSER_GROUPS, PROD_OPERATIONS_GROUPS, OPERATION_GROUPS)
-
 // match groups in token to roles
 exports.matchRoles = groups => {
   let arrRoles = ['ROLE_USER']
