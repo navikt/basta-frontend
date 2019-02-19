e2442fc1b8c691c0b77d1f494d3a87fe652f0cc3
Castigere <even.haasted@nav.no>
2019-02-19 14:36:36 +0100

    minor changes

--- frontend/src/js/containers/history/actionCreators.js
+++ frontend/src/js/containers/history/actionCreators.js
@@ -1,11 +1,7 @@
-import { HISTORY_REQUEST, HISTORY_APPLY_FILTER, HISTORY_UPDATE_REQUEST } from './actionTypes'
+import { HISTORY_REQUEST, HISTORY_APPLY_FILTER } from './actionTypes'
 
 export const getOrderHistory = (pageSize, fromDate, toDate, maxOrders) => {
   return { type: HISTORY_REQUEST, pageSize, toDate, fromDate, maxOrders }
-}
-
-export const updateOrderHistory = () => {
-  return { type: HISTORY_UPDATE_REQUEST }
 }
 
 export const applyOrderHistoryFilter = (filter, nMaxResults) => {
