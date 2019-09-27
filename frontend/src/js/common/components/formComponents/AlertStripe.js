8357a4b2aef7ed9de70239a16bc166436a704b17
matsbyfl <mats@byfuglien.net>
2019-09-27 10:22:22 +0200

    Add error alertstripe

--- frontend/src/js/common/components/formComponents/AlertStripe.js
+++ frontend/src/js/common/components/formComponents/AlertStripe.js
@@ -2,22 +2,35 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const AlertStripe = props => {
-  const { message, show } = props
+  const { message, show, type = 'info' } = props
 
   if (!show) {
     return null
   }
 
-  return (
-    <div className="formComponentGrid">
-      <div className="orderFormInfoBox">
-        <span className="orderFormInfoBoksIcon">
-          <i className="fa fa-exclamation-circle fa-2x" />
-        </span>
-        <p className="orderFormInfoBoksTekst">{message}</p>
+  if (type === 'error') {
+    return (
+      <div className="formComponentGrid">
+        <div className="alertStripe errorStripe">
+          <span className="alertStripeIcon errorIcon">
+            <i className="fa fa-times-circle fa-3x" />
+          </span>
+          <p className="alertStripeTex">{message}</p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className="formComponentGrid">
+        <div className="alertStripe infoStripe">
+          <span className="alertStripeIcon infoIcon">
+            <i className="fa fa-exclamation-circle fa-2x" />
+          </span>
+          <p className="alertStripeText">{message}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
 AlertStripe.propTypes = {
