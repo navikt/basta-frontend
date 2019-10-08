2349e62ec09043feb3514831f9bae19a2cd6d27a
matsbyfl <mats@byfuglien.net>
2019-10-08 16:48:17 +0200

    created mock response for Oracle DB orders

--- api/test/mockend/dbTemplateMock.js
+++ api/test/mockend/dbTemplateMock.js
@@ -1,12 +1,18 @@
 exports.getTemplates = () => {
   return (req, res) => {
-    res.status(200).send([
-      {
-        uri: '/em/cloud/dbaas/pluggabledbplatformtemplate/24',
-        description:
-          'Standard Production Oracle Pluggable Database, Oracle Dataguard, Backup enabled',
-        name: 'p_st_rhel_fss'
-      }
-    ])
+    console.log('req.params', req.query)
+
+    if (req.query.environmentClass === 'u') {
+      res.status(400).send('No template available for envclass u')
+    } else {
+      res.status(200).send([
+        {
+          uri: '/em/cloud/dbaas/pluggabledbplatformtemplate/24',
+          description:
+            'Standard Production Oracle Pluggable Database, Oracle Dataguard, Backup enabled',
+          name: 'p_st_rhel_fss'
+        }
+      ])
+    }
   }
 }
