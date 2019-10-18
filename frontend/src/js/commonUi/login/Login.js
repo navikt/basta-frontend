8bf3bd9a862277cdc0cc0fd26dec7ec01195aba8
matsbyfl <mats@byfuglien.net>
2019-10-18 10:28:42 +0200

    Fixed styling for logout view. Removed large basta logo and nav logo. Using smaller basta logo and the styling for button

--- frontend/src/js/commonUi/login/Login.js
+++ frontend/src/js/commonUi/login/Login.js
@@ -8,12 +8,9 @@ const Login = props => {
   return (
     <div className="login-background">
       <div className="login-container">
-        <img className="login-basta-logo" src={bastaLogo} />
-        <div className="login-box">
-          <img className="login-nav-logo" src={navLogo} />
-          <div className="login-box-header">
-            Successfully <strong>logged out</strong>
-          </div>
+        <div className="login-box" >
+          <img className="login-nav-logo" src={bastaLogo} />
+          <div className="login-box-header">Successfully logged out of Basta</div>
           <div className="login-box-body">
             <center> Ra-authenticate by logging into NAV's single sign-on provider. </center>
           </div>
