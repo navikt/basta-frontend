5c3287c109ad0b6f1e436f6a7e2ae1ffb6d86794
matsbyfl <mats@byfuglien.net>
2019-10-18 09:54:30 +0200

    Moved App to top level

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import { Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import store from './common/store'
 import history from './common/history'
-import App from './containers/App'
-
+import App from './App'
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <App/>
-        </Router>
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
 )
