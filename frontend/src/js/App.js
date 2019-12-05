9fa758df07b4bbde96fc92f4d5e1a13e22a7a39b
matsbyfl <mats@byfuglien.net>
2019-12-05 15:52:03 +0100

    Moved routes to new folder structure

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Routes } from './common/routes'
+import { Routes } from './routes'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import NavMenuSmall from './commonUi/menus/NavMenuSmall'
