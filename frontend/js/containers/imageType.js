a3352e7b5f794a2a148075a5a15b79344896ccbd
Muni10 <sten.ivar.rokke@gmail.com>
2020-02-14 10:14:59 +0100

    Switched from svg to png

--- frontend/js/containers/imageType.js
+++ frontend/js/containers/imageType.js
@@ -11,7 +11,7 @@ const bigipImage = require('../../img/orderTypes/big-ip.png')
 const mqImage = require('../../img/orderTypes/mq.png')
 const developertoolsImage = require('../../img/orderTypes/devtools.png')
 const coreosImage = require('../../img/orderTypes/containerlinux.png')
-const flatcarImage = require('../../img/orderTypes/flatcarlinux.svg')
+const flatcarImage = require('../../img/orderTypes/flatcarlinux.png')
 const unknown = require('../../img/basta.png')
 
 export default function imageType(orderDescription) {
