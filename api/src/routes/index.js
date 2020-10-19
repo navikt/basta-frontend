1c147e19adf447278f0bd122bb122f8d76cdb0aa
matsbyfl <mats@byfuglien.net>
2020-10-19 11:47:39 +0200

    prøver å logge error

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -23,6 +23,15 @@ router.get('/user/profile', auth.ensureAuthenticated(), user.getUserProfile())
 
 router.get('/user/session', auth.ensureAuthenticated(), user.userSessionLookup())
 
+router.get('/error', errorLogging())
+
 router.get('/', auth.ensureAuthenticated())
+
+const errorLogging = () => {
+  return (req, res) => {
+    console.log('Error occured', req, res)
+    res.status(500).send('Error...')
+  }
+}
 
 module.exports = router
