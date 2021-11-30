2f3322e054464632d948b162dc9e076047cad224
Muni10 <sten.ivar.rokke@nav.no>
2021-11-30 14:28:03 +0100

    Fix: Changed net binding

--- api/src/config/config.js
+++ api/src/config/config.js
@@ -1,11 +1,11 @@
 if (process.env['NODE_ENV'] === 'production') {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-  exports.host = process.env['HOST'] || 'localhost'
+  exports.host = process.env['HOST'] || '0.0.0.0'
   exports.port = process.env['PORT'] || '8080'
   exports.sessionSecret = process.env['SESSION_SECRET'] || 'H3mligereEnnDetteBlirDetIkke!'
   exports.cookieDomain = ''
 } else if (process.env['NODE_ENV'] === 'development' || process.env['NODE_ENV'] === 'offline') {
-  exports.host = 'localhost'
+  exports.host = '0.0.0.0'
   exports.port = 8080
   exports.sessionSecret = 'H3mligereEnnDetteBlirDetIkke!'
   exports.cookieDomain = ''
