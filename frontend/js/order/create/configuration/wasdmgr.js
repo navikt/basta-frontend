652fdddf9fe8b70818a350e111b49790a3b649fa
Sten Røkke <sten.ivar.rokke@nav.no>
2024-04-17 09:35:06 +0200

    feat: add rhel70 temporary for was nodes

--- frontend/js/order/create/configuration/wasdmgr.js
+++ frontend/js/order/create/configuration/wasdmgr.js
@@ -34,7 +34,10 @@ const orderFields = {
     label: 'OS version',
     description: '',
     fieldType: 'buttonGroup',
-    alternatives: [{ label: 'RHEL 8', value: 'rhel80' }],
+    alternatives: [
+      { label: 'RHEL 8', value: 'rhel80' },
+      { label: 'RHEL 7', value: 'rhel70' }
+    ],
     value: 'rhel80'
   }
 }
