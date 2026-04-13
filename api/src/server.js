50b658011085741f1041c8798220f9971cdc0a3e
Håvard Tronhus <46346413+havtro@users.noreply.github.com>
2026-04-13 17:01:28 +0200

    Attempt to fix oidc

--- api/src/server.js
+++ api/src/server.js
@@ -61,6 +61,15 @@ app.use(
   }),
 )
 app.use(passport.initialize())
+app.use((req, res, next) => {
+  if (req.session && !req.session.regenerate) {
+    req.session.regenerate = (cb) => cb()
+  }
+  if (req.session && !req.session.save) {
+    req.session.save = (cb) => cb()
+  }
+  next()
+})
 app.use(passport.session())
 
 // ROUTES
